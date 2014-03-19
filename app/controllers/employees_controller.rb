270987d4919baa9f2cffe04690f8156838999b7a
ewhitebloom <ewhitebloom@gmail.com>
2014-03-19 16:05:06 -0400

    spaces

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,9 +1,9 @@
 class EmployeesController < ApplicationController
   def index
-  @employees = Employee.all
+    @employees = Employee.all
   end
 
   def show
-  @employee = Employee.find(params[:id])
+    @employee = Employee.find(params[:id])
   end
 end
