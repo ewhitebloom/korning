c6ebf6596feceb75eab0ff2bb9bd6bd87f141bd0
ewhitebloom <ewhitebloom@gmail.com>
2014-03-19 15:00:51 -0400

    Unit_price migration

--- db/migrate/20140319183646_calculate_unit_price_for_each_product.rb
+++ db/migrate/20140319183646_calculate_unit_price_for_each_product.rb
@@ -2,11 +2,13 @@ class CalculateUnitPriceForEachProduct < ActiveRecord::Migration
   def change
     add_column :products, :unit_price, :float
 
-    Sale.reset_column_information
+    Product.reset_column_information
     Sale.find_each do |sale|
-      customer = Customer.find_by(name: sale.customer_and_account_no.split[0])
-      sale.customer_id = customer.id
-      sale.save
+      product = Product.find_by(id: sale.product_id)
+     unless product.unit_price != nil
+      sale.sale_amount / sale.units_sold = product.unit_price
+      product.save
+     end
     end
   end
 end
