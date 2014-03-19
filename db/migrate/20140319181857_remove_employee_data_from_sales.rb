1b572c7435e13b7900f5124b94d43c8041ab19b3
ewhitebloom <ewhitebloom@gmail.com>
2014-03-19 14:19:33 -0400

    remove columns from sales migrations

---
+++ db/migrate/20140319181857_remove_employee_data_from_sales.rb
@@ -0,0 +1,4 @@
+class RemoveEmployeeDataFromSales < ActiveRecord::Migration
+  def change
+  end
+end
---
+++ db/migrate/20140319181907_remove_customer_data_from_sales.rb
@@ -0,0 +1,4 @@
+class RemoveCustomerDataFromSales < ActiveRecord::Migration
+  def change
+  end
+end
