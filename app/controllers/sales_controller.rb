2f32e159d506cec92d81d4bc4f924900dc4a9cef
ewhitebloom <ewhitebloom@gmail.com>
2014-03-19 16:22:09 -0400

    views/controllers

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,9 +1,11 @@
 class SalesController < ApplicationController
   def index
-    @sales = Sale.all
+    @all_sales = Sale.joins(:unit_price).all
+    @sales = @all_sales.where(sale_date:('10-1-2012')..('12-31-2012'))
   end
 
   def show
     @sale = Sale.find(params[:id])
   end
+
 end
