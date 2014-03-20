7c88d5f474a3ff4d3aec8981661e73b14b636720
ewhitebloom <ewhitebloom@gmail.com>
2014-03-20 10:08:22 -0400

    ordered sales figures

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,9 +2,11 @@ class EmployeesController < ApplicationController
   def index
     @joins = Employee.joins(sales: [:product, :customer])
     @employees = @joins.distinct
+    @sales = @employees.sales.order(sale_amount: :desc)
   end
 
   def show
     @employee = Employee.find(params[:id])
+    @sales = @employee.sales.order(sale_amount: :desc)
   end
 end
