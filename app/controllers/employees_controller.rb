3c389fbc7da568e137da20fd090580e44736cb1a
ewhitebloom <ewhitebloom@gmail.com>
2014-03-20 10:41:55 -0400

    fixed with no ordering in index page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,7 +2,6 @@ class EmployeesController < ApplicationController
   def index
     @joins = Employee.joins(sales: [:product, :customer])
     @employees = @joins.distinct
-    @sales = @employees.sales.order(sale_amount: :desc)
   end
 
   def show
